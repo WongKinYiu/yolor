effd501404b8a5a62585f674191d0372f58e6bb6
Kin-Yiu, Wong <102582011@cc.ncu.edu.tw>
2021-05-11 20:08:17 +0800

    Update get_pretrain.sh

--- scripts/get_pretrain.sh
+++ scripts/get_pretrain.sh
@@ -1,3 +1,15 @@
-curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=1Tdn3yqpZ79X7R1Ql0zNlNScB1Dv9Fp76" > /dev/null
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=1Tdn3yqpZ79X7R1Ql0zNlNScB1Dv9Fp76" -o yolor_p6.pt
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=1WyzcN1-I0n8BoeRhi_xVt8C5msqdx_7k" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=1WyzcN1-I0n8BoeRhi_xVt8C5msqdx_7k" -o yolor-p6.pt
+rm ./cookie
+
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=1KnkBzNxATKK8AiDXrW_qF-vRNOsICV0B" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=1KnkBzNxATKK8AiDXrW_qF-vRNOsICV0B" -o yolor-w6.pt
+rm ./cookie
+
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=1jVrq8R1TA60XTUEqqljxAPlt0M_MAGC8" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=1jVrq8R1TA60XTUEqqljxAPlt0M_MAGC8" -o yolor-e6.pt
+rm ./cookie
+
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=1WX33ymg_XJLUJdoSf5oUYGHAtpSG2gj8" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=1WX33ymg_XJLUJdoSf5oUYGHAtpSG2gj8" -o yolor-d6.pt
 rm ./cookie
