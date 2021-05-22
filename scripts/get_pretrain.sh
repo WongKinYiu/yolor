0c42593d717757252c5048e68a0fbf453920fe9f
Kin-Yiu, Wong <102582011@cc.ncu.edu.tw>
2021-05-22 15:50:55 +0800

    Delete scripts directory

--- scripts/get_pretrain.sh
+++
@@ -1,3 +0,0 @@
-curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=1Tdn3yqpZ79X7R1Ql0zNlNScB1Dv9Fp76" > /dev/null
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=1Tdn3yqpZ79X7R1Ql0zNlNScB1Dv9Fp76" -o yolor_p6.pt
-rm ./cookie
