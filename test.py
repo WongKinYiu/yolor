e8afde99a607c466492f7b5910ddb7086afc534b
HERIUN <ehdrndd@naver.com>
2023-06-09 11:17:39 +0900

    Update test.py
    W&B logging TypeError: list indices must be integers or slices, not float error.

--- test.py
+++ test.py
@@ -162,7 +162,7 @@ def test(data,
             if plots and len(wandb_images) < log_imgs:
                 box_data = [{"position": {"minX": xyxy[0], "minY": xyxy[1], "maxX": xyxy[2], "maxY": xyxy[3]},
                              "class_id": int(cls),
-                             "box_caption": "%s %.3f" % (names[cls], conf),
+                             "box_caption": "%s %.3f" % (names[int(cls)], conf),
                              "scores": {"class_score": conf},
                              "domain": "pixel"} for *xyxy, conf, cls in pred.tolist()]
                 boxes = {"predictions": {"box_data": box_data, "class_labels": names}}
